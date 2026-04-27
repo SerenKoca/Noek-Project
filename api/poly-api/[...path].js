46c77eacbc7eb4338ef1d630d598e2f39158efe5
SerenKoca <r0937023@student.thomasmore.be>
2026-04-27 23:20:05 +0200

    fix: poly pizza error try 6

--- api/poly-api/[...path].js
+++
@@ -1,5 +0,0 @@
-import { polyRouter } from '../../src/server/polyApi/routes/polyRoutes.js'
-
-export default async function handler(req, res) {
-  await polyRouter(req, res)
-}
--- api/poly-static/[...path].js
+++
@@ -1,5 +0,0 @@
-import { staticRouter } from '../../src/server/polyStatic/routes/staticRoutes.js'
-
-export default async function handler(req, res) {
-  await staticRouter(req, res)
-}
